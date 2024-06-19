329510d5559a77a82460213560f879560ec8fbdb
pythonninjax00 <170425268+pythonninjax00@users.noreply.github.com>
2024-06-19 20:11:26 +0530

    Add files via upload
    playback.py
    This Python program simulates a slower playback speed by replacing each space in the user's input with three periods (...). It prompts the user for a string and outputs the modified string with the added pauses between words. This project is a solution to the CS50 problem set and demonstrates basic string manipulation and user input handling in Python.

---
+++ playback.py
@@ -0,0 +1,10 @@
+# ask user for any input
+Sentence= input("Write anything")
+
+# split the sentence
+Sentencee =Sentence.split()
+#print the output adding three dots in the output
+print("...".join(Sentencee)) 
+
+
+
