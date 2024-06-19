40fb245e443f1489dcfbfad2e5e637474eaa8da3
pythonninjax00 <170425268+pythonninjax00@users.noreply.github.com>
2024-06-19 20:17:26 +0530

    Add files via upload
    einstein.py
    This Python program calculates the energy equivalent of a given mass using Einstein's famous equation 
    E=mc^2
     .It prompts the user to input a mass in kilograms, then computes and outputs the corresponding energy in Joules. The speed of light 
    𝑐 is approximated as 
    300,000,000 meters per second. This project is a solution to the CS50 problem set and demonstrates basic arithmetic operations and user input handling in Python.

---
+++ einstein.py
@@ -0,0 +1,6 @@
+# ask user for a integer value of value of mass in kilograms
+M = input("m:")
+M = int(M)
+#convert m into e which is in joules by multiplying it by 90000000000000000
+e = int(M * 90000000000000000)
+print(e)
